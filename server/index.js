e2c656dcd9f8f3a906fc5cb6b190a7d64ab68235
tharinda-oxx <tharindadecruzz.sa@gmail.com>
2021-08-17 12:49:17 +0530

    backend develoment

--- server/index.js
+++ server/index.js
@@ -1,4 +1,6 @@
 const express = require("express");
+const bodyParser = require("body-parser")
+const cors = require("cors");
 const app = express();
 const mysql = require("mysql");
 
@@ -8,11 +10,25 @@ const db = mysql.createPool({
      password:"admin",
      database:"employee_system"
 });
+    app.use(cors());
+    app.use(express.json());
+    app.use(bodyParser.urlencoded({extended:true}));
+app.post("/api/insert", (req, res) =>{
 
-app.get("/", (req, res) =>{
-    const sqlInsert = "INSERT INTO employee(full_name,name_with_in,birth_day,age,address,city,telephone_no,email,department,job_roll)VALUES('test1','Test2','1998-06-24',23,'aaaaaa','city','0716727703','test@gmail.com','HR','HRM');";
-    db.query(sqlInsert,(err, result )=>{
-        res.send(result);
+       const full_name = req.body.full_name;
+       const name_with_in = req.body.name_with_in;
+       const birth_day = req.body.birth_day;
+       const age = req.body.age;
+       const address = req.body.address;
+       const city = req.body.city;
+       const telephone_no = req.body.telephone_no;
+       const email =req.body.email;
+       const department = req.body.department;
+       const job_roll = req.body.job_roll;
+
+    const sqlInsert = "INSERT INTO employee(full_name,name_with_in,birth_day,age,address,city,telephone_no,email,department,job_roll)VALUES(?,?,?,?,?,?,?,?,?,?);";
+    db.query(sqlInsert, (full_name,name_with_in,birth_day,age,address,city,telephone_no,email,department,job_roll),(err, result )=>{
+        console.log(result);
     });
    
 });
